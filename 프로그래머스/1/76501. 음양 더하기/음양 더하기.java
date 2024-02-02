0d0bdc5e3787b7e1d97ff21484fd7c80d5d853f7
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-02-02 09:55:54 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i < absolutes.length; i++){
+            int num = absolutes[i];
+            
+            if(signs[i] == false){
+                num *= -1;
+            }
+            
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
