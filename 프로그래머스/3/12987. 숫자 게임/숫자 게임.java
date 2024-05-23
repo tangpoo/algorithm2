0b07fbcb8daef4ebf44264c5b7ab432382cceee9
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-05-23 19:47:15 +0900

    [level 3] Title: 숫자 게임, Time: 65.99 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int aIdx = 0;
+        int bIdx = 0;
+        while(aIdx < A.length && bIdx < B.length){
+            if(A[aIdx] < B[bIdx]){
+                answer++;
+                aIdx++;
+            }
+            bIdx++;
+        }
+        
+        return answer;
+    }
+}
