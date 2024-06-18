7c1fdc0365c1909b993571573ba774c900ef92c9
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-06-18 12:19:44 +0900

    [level 3] Title: 입국심사, Time: 0.39 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long left = 0, right = times[times.length - 1] * (long) n;
+        
+        while(left <= right){
+            long mid = (left + right) / 2;
+            
+            long complete = 0;
+            for(int i = 0; i < times.length; i++){
+                complete += mid / times[i];
+            }
+            
+            if(complete < n){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+                answer = mid;
+            }
+        }
+        return answer;
+    }
+}
