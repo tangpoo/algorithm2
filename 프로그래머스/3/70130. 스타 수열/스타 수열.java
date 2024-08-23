86e7ae760fbbbb3ae0666b4c0b0513b8765b5ecd
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-08-23 22:42:52 +0900

    [level 3] Title: 스타 수열, Time: 31.96 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -3,23 +3,22 @@ class Solution {
         int answer = 0;
         int[] count = new int[a.length];
         
-        for(int i = 0; i < a.length; i++){
+        for(int i = 0; i < a.length; i++) {
             count[a[i]]++;
         }
         
-        
-        for(int i = 0; i < a.length; i++){
-            if(count[i] == 0) continue;
+        for(int i = 0; i < a.length; i++) {
             if(count[i] <= answer) continue;
             
             int ret = 0;
-            for(int j = 0; j < a.length - 1; j++){
-                if(a[j] == a[j+1]) continue;
-                if(a[j] != i && a[j+1] != i) continue;
+            for(int j = 0; j < a.length - 1; j++) {
+                if(a[j] == a[j + 1]) continue;
+                if(a[j] != i && a[j + 1] != i) continue;
                 
                 ret++;
                 j++;
             }
+        
             answer = Math.max(answer, ret);
         }
         
