c07b1d5cd709750c98a011fbc4c71c96cf79777a
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-08-24 22:16:43 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 1169.74 ms, Memory: 232 MB -BaekjoonHub

--- 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.java
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.java
@@ -1,43 +1,48 @@
 import java.util.*;
 class Solution {
     
+    static long[] copyA;
     static ArrayList<Integer>[] list;
     static boolean[] visit;
-    static long[] long_a;
     static long answer = 0;
     
     public long solution(int[] a, int[][] edges) {        
+        copyA = new long[a.length];
         list = new ArrayList[a.length];
         visit = new boolean[a.length];
-        long_a = new long[a.length];
-        long sum = 0;
+        int sum = 0;
         
-        for(int i = 0; i < a.length; i++){
+        for(int i = 0; i < a.length; i++) {
             sum += a[i];
-            long_a[i] = a[i];
-            list[i] = new ArrayList<>();
+            copyA[i] = a[i];
+            list[i] = new ArrayList();
         }
+        
         if(sum != 0) return -1;
         
-        for(int[] i : edges){
-            list[i[0]].add(i[1]);
-            list[i[1]].add(i[0]);
+        for(int i = 0; i < edges.length; i++) {
+            list[edges[i][1]].add(edges[i][0]);
+            list[edges[i][0]].add(edges[i][1]);
         }
+        
         dfs(0);
         
         return answer;
+        
     }
     
-    static long dfs(int v){
+    static long dfs(int v) {
         visit[v] = true;
-        for(int i = 0; i < list[v].size(); i++){
-                int next = list[v].get(i);
-                if(!visit[next]){
-                    long_a[v] += dfs(next);
-                }
-            }
-        answer += Math.abs(long_a[v]);
         
-        return long_a[v];
+        for(int i = 0; i < list[v].size(); i++) {
+            int next = list[v].get(i);
+            
+            if(!visit[next]) {
+                copyA[v] += dfs(next);
+            }
+        }
+        answer += Math.abs(copyA[v]);
+        
+        return copyA[v];
     }
 }
