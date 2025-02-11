5a01d8b82a28c6c1ba977e2d69e2827a52cb1fde
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-02-12 01:29:58 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 68216 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n];
+        dfs(n, m, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (m == depth) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(n, m, depth + 1);
+        }
+    }
+}
