d9834daf13b3cfc515b76687c08b1416981944a6
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-02-11 17:10:22 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,25 +5,26 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         int[] sum = new int[n + 1];
-        
+
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         Arrays.sort(arr);
-        
-        for(int i = 1; i <= n; i++){
+
+        for (int i = 1; i <= n; i++) {
             sum[i] = sum[i - 1] + arr[i - 1];
         }
-        
-        for(int i = 1; i <= n; i++) {
-        	sum[i] += sum[i - 1];
+
+        for (int i = 1; i <= n; i++) {
+            sum[i] += sum[i - 1];
         }
+
         System.out.println(sum[n]);
     }
 }
