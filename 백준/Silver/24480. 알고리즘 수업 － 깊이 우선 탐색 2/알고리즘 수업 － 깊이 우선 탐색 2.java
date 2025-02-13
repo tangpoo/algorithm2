3c26cd3f206219047504f0c7578375c0231d6f0b
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-02-13 21:54:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 924 ms, Memory: 90228 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,61 +1,60 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
-    
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static int[] check;
+class Main {
+    static ArrayList<ArrayList<Integer>> arr;
+    static int[] result;
+    static int n;
     static int count;
-    
-    public static void main(String[] args) throws IOException{
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        
-        int n = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
-        check = new int[n + 1];
-        
-        for(int i = 0; i < n + 1; i++){
-            graph.add(new ArrayList<>());
+        int start = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList<>();
+        result = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            arr.add(new ArrayList<>());
         }
-        
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr.get(a).add(b);
+            arr.get(b).add(a);
         }
-        
-        for(int i = 1; i < graph.size(); i++){
-            Collections.sort(graph.get(i), Collections.reverseOrder());
+
+        for (int i = 0; i < arr.size(); i++) {
+            Collections.sort(arr.get(i), Collections.reverseOrder());
         }
-        
+
         count = 1;
-        
-        dfs(r);
-        
-        for(int i = 1; i < check.length; i++){
-            sb.append(check[i]).append('\n');
+        dfs(start);
+
+        for (int i = 1; i < result.length; i++) {
+            sb.append(result[i]).append('\n');
         }
         System.out.println(sb);
     }
-    
-    static void dfs(int vertex){
-        check[vertex] = count;
-        
-        for(int i = 0; i < graph.get(vertex).size(); i++){
-            int newVertex = graph.get(vertex).get(i);
-            
-            if(check[newVertex] == 0){
+
+    private static void dfs(int idx) {
+        result[idx] = count;
+
+        for (int i = 0; i < arr.get(idx).size(); i++) {
+            int newIdx = arr.get(idx).get(i);
+
+            if (result[newIdx] == 0) {
                 count++;
-                dfs(newVertex);
+                dfs(newIdx);
             }
         }
     }
 }
-
