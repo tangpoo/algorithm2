0453b42ca9f1c167fc436d2b06ae9eb37361c531
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-02-13 22:19:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 924 ms, Memory: 89312 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,64 +1,64 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static int[] visited;
-    static int N, M;
-    
-    public static void main(String[] args) throws IOException{
+class Main {
+    static ArrayList<ArrayList<Integer>> arr;
+    static int[] result;
+    static int n;
+    static int count;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
-        
-        visited = new int[N + 1];
-        
-        for(int i = 0; i <= N; i++) {
-        	graph.add(new ArrayList<>());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList<>();
+        result = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            arr.add(new ArrayList<>());
         }
-        
-        for(int i = 0; i < M; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-        	
-        	int A = Integer.parseInt(st.nextToken());
-        	int B = Integer.parseInt(st.nextToken());
-        	
-        	graph.get(A).add(B);
-        	graph.get(B).add(A);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr.get(a).add(b);
+            arr.get(b).add(a);
         }
-        
-        for(int i = 1; i <= N; i++) {
-        	Collections.sort(graph.get(i));
+
+        for (int i = 0; i < arr.size(); i++) {
+            Collections.sort(arr.get(i));
         }
-        
-        bfs(R);
-        
-        for(int i = 1; i <= N; i++) {
-        	System.out.println(visited[i]);
+
+        count = 1;
+        bfs(start);
+
+        for (int i = 1; i < result.length; i++) {
+            sb.append(result[i]).append('\n');
         }
+        System.out.println(sb);
     }
-    
-    static void bfs(int start) {
-    	Queue<Integer> q = new LinkedList<>();
-    	int cnt = 1;
-    	
-    	q.offer(start);
-    	visited[start] = cnt++;
-    	
-    	while(!q.isEmpty()) {
-    		int a = q.poll();
-    		
-    		for(int i = 0; i < graph.get(a).size(); i++) {
-    			int nextV = graph.get(a).get(i);
-    			
-    			if(visited[nextV] == 0) {
-    				q.offer(nextV);
-        			visited[nextV] = cnt++;
-    			}  			
-    		}
-    	}
+
+    private static void bfs(int idx) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(idx);
+        result[idx] = count++;
+
+        while(!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int val : arr.get(node)) {
+                if (result[val] == 0) {
+                    q.add(val);
+                    result[val] = count++;
+                }
+            }
+        }
     }
 }
