766fcd910d208b31c8130c2c653c3cdab5fdd7a1
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-02-14 16:01:51 +0900

    [Gold V] Title: 토마토, Time: 544 ms, Memory: 119828 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,81 +1,67 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
-    
-    static int[][] map;
-    static int[] dirX = {0, 0, -1, 1};
-    static int[] dirY = {-1, 1, 0, 0};
-    static int N, M;
-    static Queue<int[]> q= new LinkedList<>(); 
-    
-    public static void main(String[] args) throws IOException{
+class Main {
+
+    static int[][] tomato;
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
+    static int n;
+    static int m;
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        
-        map = new int[N][M];
-        
-        for (int i = 0; i < N; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        tomato = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                
-                if (map[i][j] == 1) {
+            for (int j = 0; j < m; j++) {
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 1) {
                     q.add(new int[]{i, j});
                 }
             }
         }
-        
-        System.out.println(bfs());
+
+        bfs();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tomato[i][j] == 0) {
+                    System.out.println(-1);
+                    System.exit(0);
+                }
+
+                if (max < tomato[i][j]) {
+                    max = tomato[i][j];
+                }
+            }
+        }
+
+        System.out.println(max - 1);
     }
-    
-    private static int bfs() {
-    	while(!q.isEmpty()) {
-    		int[] t = q.poll();
-    		int x = t[0];
-    		int y = t[1];
-    		
-    		for(int i = 0; i < 4; i++) {
-    			int nx = x + dirX[i];
-    			int ny = y + dirY[i];
-    			
-    			if(nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == 0) {
-    				map[nx][ny] = map[x][y] + 1;
-    				q.add(new int[] {nx, ny});
-    			}  			
-    		}
-    	}
-    	
-    	int max = Integer.MIN_VALUE;
-    	
-    	if(checkZero()) {
-    		return -1;   		
-    	}
-    	else {
-    		for(int i = 0; i < N; i++) {
-    			for(int j = 0; j < M; j++) {
-    				if(max < map[i][j]) {
-    					max = map[i][j];
-    				}
-    			}
-    		}
-    		return max - 1;
-    	}
-    }
-    
-    private static boolean checkZero() {
-    	for(int i = 0; i < N; i++) {
-    		for(int j = 0; j < M; j++) {
-    			if(map[i][j] == 0) {
-    				return true;
-    			}
-    		}    		
-    	}
-    	return false;
+
+    static void bfs() {
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dirX[i];
+                int ny = y + dirY[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && tomato[nx][ny] == 0) {
+                    q.add(new int[]{nx, ny});
+                    tomato[nx][ny] = tomato[x][y] + 1;
+                }
+            }
+        }
     }
 }
